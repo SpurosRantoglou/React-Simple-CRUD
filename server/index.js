a84c33ce70c91041cb68b16c4a3b8e737b6f6167
Spuros <spurakos98@gmail.com>
2021-01-24 00:01:38 +0200

    [Server] Set up connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,35 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
-app.listen(3001, ()=>{console.log("Running on port 3010")});
+//Get connection with the db
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password:'password',
+    database: 'employeesystem'
+})
+
+
+//Insert
+app.post('/create', (req, res)=>{
+
+    const name = req.body.name
+    const age = req.body.age
+    const country = req.body.country
+    const position = req.body.position
+    const wage = req.body.wage
+
+    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES(?,?,?,?,?)',
+     [name,age,country, position, wage], (error, result)=>{
+         if(error){
+             console.log(error)
+         }else{
+             res.send("Values Inserted")
+         }
+     });
+
+});
+
+
+app.listen(3310, ()=>{console.log("Running on 3001")});
